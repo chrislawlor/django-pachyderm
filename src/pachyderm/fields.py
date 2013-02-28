8c44e11cae15b2683f45a473d2b79abdaee83a62
Chris Lawlor <lawlor.chris@gmail.com>
2013-02-28 16:20:37 -0500

    PEP8

--- src/pachyderm/fields.py
+++ src/pachyderm/fields.py
@@ -3,35 +3,41 @@ from django.core.exceptions import ImproperlyConfigured, ValidationError
 
 
 class ArrayField(models.Field):
-    
+
     description = """
     PostgreSQL allows columns of a table to be defined as variable-length
     multidimensional arrays. Arrays of any built-in or user-defined base
     type, enum type, or composite type can be created."""
-    
+
     __metaclass__ = models.SubfieldBase
-    
+
     def __init__(self, verbose_name=None, name=None,
                  dimensions=1, data_type=None, **kwargs):
         self.dimensions = dimensions
         self.data_type = data_type
-        
+
         if not type(self.dimensions) == int:
-            raise ImproperlyConfigured("ArrayField dimensions must be an integer, but is %s" % self.dimensions)
+            raise ImproperlyConfigured(
+                "ArrayField dimensions must be an integer, but is %s"
+                % self.dimensions)
+
         if not self.dimensions > 0:
-            raise ImproperlyConfigured("ArrayField dimensions must be greater than zero")
-        
+            raise ImproperlyConfigured(
+                "ArrayField dimensions must be greater than zero")
+
         if kwargs.get('primary_key'):
-            raise ImproperlyConfigured("ArrayField cannot be a primary key.")
-        
+            raise ImproperlyConfigured(
+                "ArrayField cannot be a primary key.")
+
         if data_type is None:
-            raise ImproperlyConfigured("ArrayField requires data_type to be specified.")
-        
+            raise ImproperlyConfigured(
+                "ArrayField requires data_type to be specified.")
+
         models.Field.__init__(self, verbose_name, name, **kwargs)
-    
+
     def db_type(self, connection):
         return "%s%s" % (self.data_type, "[]" * self.dimensions)
-    
+
     def to_python(self, value):
         if isinstance(value, list):
             return value
--- tests/test_app/tests.py
+++ tests/test_app/tests.py
@@ -22,38 +22,40 @@ def psycopg2_version_gte(version_string):
 
 class ArrayFieldTests(TransactionTestCase):
     def test_adding_rows(self):
-        container = TextArrayContainer.objects.create(name='django', items=['framework', 'python'])
+        container = TextArrayContainer.objects.create(
+            name='django', items=['framework', 'python'])
         self.assertEqual(1, TextArrayContainer.objects.all().count())
         retrieved = TextArrayContainer.objects.get(name='django')
         self.assertEqual(container.items, retrieved.items)
         self.assertEqual(2, len(retrieved.items))
         self.assertEqual(container.items[0], retrieved.items[0])
         self.assertEqual(container.items[1], retrieved.items[1])
-    
+
     def test_contains_array_element(self):
-        TextArrayContainer.objects.create(name='django', items=['framework', 'python'])
-        self.assertEqual(1, TextArrayContainer.objects.filter(items__contains='framework').count())
-        self.assertEqual(0, TextArrayContainer.objects.filter(items__contains='php').count())
-    
+        TextArrayContainer.objects.create(
+            name='django', items=['framework', 'python'])
+        self.assertEqual(1, TextArrayContainer.objects.filter(
+            items__contains='framework').count())
+        self.assertEqual(0, TextArrayContainer.objects.filter(
+            items__contains='php').count())
+
     def test_integer_type(self):
-        container = IntegerArrayContainer.objects.create(name='container', items=[1,2,3,4,5])
+        container = IntegerArrayContainer.objects.create(
+            name='container', items=[1, 2, 3, 4, 5])
         self.assertEqual(1, IntegerArrayContainer.objects.all().count())
         retrieved = IntegerArrayContainer.objects.get(name='container')
         self.assertEqual(container.items, retrieved.items)
         self.assertEqual(5, len(retrieved.items))
-        self.assertEqual(1, IntegerArrayContainer.objects.filter(items__contains=1).count())
-    
-    #def test_text_in_integer_array(self):
-    #    with self.assertRaises(DatabaseError):
-    #        container = IntegerArrayContainer.objects.create(name='container', items=['1', '2', '3'])
-    #    self.assertEqual(0, IntegerArrayContainer.objects.all().count())
-    
-    @unittest.skipUnless(psycopg2_version_gte('2.4.5'), 'Array support for Inet type introduced in psycopg2 version 2.4.5')
+        self.assertEqual(1, IntegerArrayContainer.objects.filter(
+            items__contains=1).count())
+
+    @unittest.skipUnless(psycopg2_version_gte('2.4.5'),
+                         'Array support for Inet introduced in psycopg2 2.4.5')
     def test_inet_type(self):
-        container = IPArrayContainer.objects.create(name='server1', items=[Inet('10.10.10.10'), Inet('10.10.10.11')])
+        container = IPArrayContainer.objects.create(
+            name='server1', items=[Inet('10.10.10.10'), Inet('10.10.10.11')])
         self.assertEqual(1, IPArrayContainer.objects.all().count())
         retrieved = IPArrayContainer.objects.get(name='server1')
         # psycopg2's Inet does not support direct comparisons, i.e.
         # Inet('10.10.10.10') == Inet('10.10.10.10') is False
         self.assertEqual('10.10.10.10', retrieved.items[0].addr)
-
