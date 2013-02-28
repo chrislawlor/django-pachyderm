700f222901fd1310a92614d8b2fb5b29661bc53a
Chris Lawlor <lawlor.chris@gmail.com>
2013-02-28 15:19:56 -0500

    Add some type-specific tests

--- tests/test_app/models.py
+++ tests/test_app/models.py
@@ -1,10 +1,29 @@
 from django.db import models
+from psycopg2.extras import register_inet
+
 from pachyderm import ArrayField
 
 
-class Item(models.Model):
+class NamedModel(models.Model):
     name = models.CharField(max_length=20)
-    tags = ArrayField(dimensions=1, data_type='text')
+    
+    class Meta:
+        abstract = True
     
     def __unicode__(self):
         return self.name
+
+
+class TextArrayContainer(NamedModel):
+    items = ArrayField(dimensions=1, data_type='text')
+
+
+class IntegerArrayContainer(NamedModel):
+    items = ArrayField(dimensions=1, data_type='integer')
+
+
+register_inet()
+class IPArrayContainer(NamedModel):
+    items = ArrayField(dimensions=1, data_type='inet')
+
+
